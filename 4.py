4ce7377769c5c25a00e750a2f2c2274666f0c8ef
Arthur Gomes <arthurcosta007110@gmail.com>
2024-09-12 17:29:12 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,37 @@
+def factor(n):
+    factors = []
+
+    count = 0
+    while n % 2 == 0:
+        count += 1
+        n //= 2
+    if count > 0:
+        factors.append((2, count))
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        count = 0
+        while n % i == 0:
+            count += 1
+            n //= i
+        if count > 0:
+            factors.append((i, count))
+
+    if n > 2:
+        factors.append((n, 1))
+
+    return factors
+
+
+def euler_totient(n):
+    factors = factor(n)
+
+    phi = n
+
+    for p, _ in factors:
+        phi *= (1 - 1/p)
+
+    return int(phi)
+
+
+n = 36
+print(euler_totient(n))
